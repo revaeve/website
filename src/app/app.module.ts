fe386c05bf489f5e49aca43026d5d84deeabd84d
Anastasiia Vatsyk <58269603+avgusta92@users.noreply.github.com>
2020-09-20 20:01:33 +0300

    First primeNg commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { ButtonModule } from "primeng/button";
+import { CardModule } from 'primeng/card';
+import { TabViewModule } from "primeng/tabview";
+import { MegaMenuModule } from "primeng/megamenu";
 
 @NgModule({
   declarations: [
@@ -10,7 +16,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    CardModule,
+    TabViewModule,
+    MegaMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
